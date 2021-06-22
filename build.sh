ca0453a6770124d9455d21ab0f70cb0a7ff5f377
Jon Lund Steffensen <jonlst@gmail.com>
2021-06-22 01:52:34 -0700

    Update build.sh for columbus-5

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-docker run --rm -v "$(pwd)":/code   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry   cosmwasm/workspace-optimizer:0.10.3
+docker run --rm \
+  -v "$(pwd)":/code \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/workspace-optimizer:0.11.3
